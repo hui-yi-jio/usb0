840639fd2804e744a9c94fa5803fdb1f0edc4e6a
user664635 <myy12358@gmail.com>
2025-11-06 22:11:01 +0800

    usbto4port_tmp

--- uart.sv
+++ uart.sv
@@ -2,11 +2,11 @@ module uart(
 	input clk,
 	input txact,txpop,
 	input [3:0]endpt,
-	output reg txval,txcork,
+	output txval,txcork,
 	output reg [7:0]txdat,
 	output reg [11:0]txdat_len,
 	input rxact,rxval,
-	output reg rxrdy,
+	output rxrdy,
 	input reg [7:0]rxdat
 );
 wire [5:0]compare;
@@ -14,18 +14,21 @@ reg [5:0]ri = 'b0;
 reg [5:0]ti = 'b0;
 reg [7:0][0:63]tmp;
 assign compare = ri-ti;
-	always@ (posedge clk)begin
-		txcork <= 0;//compare ? 0 : 1;
-		if(txact)begin
-			txdat <= ri;//tmp[ti];
-			txdat_len <= 1;
-			ti <= ti + 1;
-		end
-		rxrdy <= 1;
-			
-		if(rxact && rxval && (endpt==0))begin
+assign txcork = 0;//compare ? 0 : 1;
+assign txval = 0;
+assign rxrdy = 1;
+	always @(posedge txact)begin
+		case(endpt)
+			2:begin
+				txdat <= ri;//tmp[ti];
+				txdat_len <= 1;
+				ti <= ti + 1;
+			end
+			default:;
+		endcase
+	end
+	always @(posedge rxval)begin
 			ri <= ri + 1;
 			tmp[ri] <= rxdat;
-		end
 	end
 endmodule
